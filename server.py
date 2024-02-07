02848c00e3fdf4949cfe602182dacb07902e7547
Aditi Agarwal <aditiagarwal0125@gamil.com>
2024-02-07 23:57:53 +0530

    Deleted the commented code form server.py

--- server.py
+++ server.py
@@ -1,37 +1,3 @@
-# from fastapi import FastAPI
-# import uvicorn
-# from Model import initialize_llm, process_pdf, process_prompt
-
-# app = FastAPI()
-
-# # Initialize the language model and processing chain
-# initialize_llm()
-
-# @app.get("/")
-# def check():
-#     return {"message": "Hello World!"}
-
-# # Define API endpoints
-# @app.post("/upload-pdf")
-# async def upload_pdf():
-#     # Specify the path to the PDF file
-#     pdf_path = "data/DeepLearning.pdf"
-
-#     # Process the PDF and build the retrieval chain
-#     process_pdf(pdf_path)
-
-#     return {"message": "PDF processed successfully"}
-
-# @app.post("/prompt")
-# async def process_user_prompt(prompt: str):
-#     # Process user prompt and return response
-#     response = process_prompt(prompt)
-#     return {"response": response}
-
-# if __name__ == "_main_":
-    
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
-
 from fastapi import FastAPI
 import uvicorn
 from Model import initialize_llm, process_pdf, process_prompt
